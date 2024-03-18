77ddaec25a85bb587b0ff34758dbf0e559a97084
Wilfred Lemayian <wilfred.lemayian@gmail.com>
2024-03-18 07:22:38 +0300

    Arrays Lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your solution here!
+beforeEach(function () {
+    // Clear the cats array by setting its length to 0
+    cats.length = 0;
+  
+    // Populate the cats array with three cat names: "Milo", "Otis", and "Garfield"
+    cats.push("Milo", "Otis", "Garfield");
+  });
